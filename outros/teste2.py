f023b55cce3b9bbc9e58828fe95e1ce33e2ed136
italomontee <igm.eng22@uea.edu.br>
2023-08-09 22:49:33 -0400

    otimização e gerando exe

--- outros/teste2.py
+++
@@ -1,6 +0,0 @@
-result_str = ""  # Inicializa a variável vazia
-
-for i in range(1, 6):  # Exemplo de loop de 1 a 5
-    result_str += f"Valor {i}: {i * 2}\n"  # Concatena o novo valor ao resultado
-
-print(result_str)  # Imprime o resultado final
